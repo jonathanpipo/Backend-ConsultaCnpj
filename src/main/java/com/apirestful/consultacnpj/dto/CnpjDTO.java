16c61dfc47bb6159e234e7c57cf2c11f2f2ae407
Jota P <89938164+jonathanpipo@users.noreply.github.com>
2024-08-26 10:25:05 -0300

    Update CnpjDTO.java

--- src/main/java/com/apirestful/consultacnpj/dto/CnpjDTO.java
+++ src/main/java/com/apirestful/consultacnpj/dto/CnpjDTO.java
@@ -4,6 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 //TODO Fix: Null returns in json
 public record CnpjDTO(@JsonProperty("cnpj_raiz") String cnpjRaiz,
-                      @JsonProperty("razao_social")String razaoSocial,
-                      @JsonProperty("capital_social")String capitalSocial) {
+                      @JsonProperty("razao_social") String razaoSocial,
+                      @JsonProperty("capital_social") String capitalSocial) {
 }
